45cd175cb5fd8e46e4fc44fe9478bc99bb9ee50e
Travis Alstrand <travisalstrand@gmail.com>
2024-01-12 12:08:48 -0800

    mobile header layout set

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,27 @@
+import hamburgerIcon from "/icons/bars-solid.svg";
+
 export default function Header() {
   return (
     <>
-      <header>
-        <p>Schaut Construction</p>
+      <header className="flex items-center bg-grayDark px-8 shadow-xl">
+        {/* empty left section to center logo on mobile */}
+        <div className="flex-1"></div>
+        <div className="flex flex-1 justify-center">
+          <img
+            src="/logo/logo_white.png"
+            alt="Schaut Construction Logo"
+            className="w-16 py-4"
+          />
+        </div>
+        <div className="flex flex-1 justify-end">
+          <button>
+            <img
+              src={hamburgerIcon}
+              alt="Mobile navigation open icon"
+              className="w-6"
+            />
+          </button>
+        </div>
       </header>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -51,6 +51,15 @@ module.exports = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        grayDark: {
+          DEFAULT: "#333333",
+        },
+        grayLight: {
+          DEFAULT: "#686868",
+        },
+        whiteLight: {
+          DEFAULT: "#F2F2F2",
+        },
       },
       borderRadius: {
         lg: "var(--radius)",
@@ -74,4 +83,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
