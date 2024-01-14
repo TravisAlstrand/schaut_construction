ed58fbf7003c2b8b60b3c52e0f7bbc9394a8991e
Travis Alstrand <travisalstrand@gmail.com>
2024-01-14 11:16:12 -0800

    added carousel to Gallery section

--- src/components/page_sections/Gallery.tsx
+++ src/components/page_sections/Gallery.tsx
@@ -1,9 +1,43 @@
+import Autoplay from "embla-carousel-autoplay";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
 import SectionHeading from "../SectionHeading";
+
+import { images } from "../../json/carouselImages.json";
 
 export default function Gallery() {
   return (
     <section id="gallery" className="my-16 px-4">
       <SectionHeading headingTitle="Gallery" headingColor="grayDark" />
+      <Carousel
+        opts={{
+          align: "start",
+          loop: true,
+        }}
+        plugins={[
+          Autoplay({
+            delay: 10000,
+          }),
+        ]}
+      >
+        <CarouselContent>
+          {images.map((image, index) => {
+            return (
+              <CarouselItem key={index}>
+                <img src={image.imgUrl} alt={image.imgAltText} />
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+        <CarouselPrevious className="left-2" />
+        <CarouselNext className="right-2" />
+      </Carousel>
     </section>
   );
 }
