4afc350b6d18b1e0ffffee1a68c6dac62efd5936
Travis Alstrand <travisalstrand@gmail.com>
2024-01-15 10:32:44 -0800

    contact form working, set up with EmailJS

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,6 +1,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import emailjs from "@emailjs/browser";
 
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
@@ -12,7 +13,7 @@ import {
   FormField,
   FormItem,
   FormLabel,
-  // FormMessage,
+  FormMessage,
 } from "@/components/ui/form";
 
 const formSchema = z.object({
@@ -22,17 +23,34 @@ const formSchema = z.object({
   email: z.string().email({
     message: "Must be a valid email address",
   }),
-  phone: z.number().min(9).max(10),
+  phone: z.string().min(10),
   message: z.string().min(5),
 });
 
 export default function ContactForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullName: "",
+      email: "",
+      phone: "",
+      message: "",
+    },
   });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+    // console.log(values);
+
+    emailjs
+      .send("service_n4ju0hr", "template_ahyqx1g", values, "6g0rq8vN720O74Ji5")
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        },
+      );
   }
 
   return (
@@ -89,6 +107,7 @@ export default function ContactForm() {
             </FormItem>
           )}
         />
+        <FormMessage />
         <Button type="submit" className="w-full bg-grayDark text-whiteLight">
           Submit
         </Button>
