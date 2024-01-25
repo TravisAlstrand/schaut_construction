6842eb086599031bd79b5ca32dca6d861cc8e383
Travis Alstrand <travisalstrand@gmail.com>
2024-01-25 15:15:22 -0800

    hero img max height

--- src/components/page_sections/Hero.tsx
+++ src/components/page_sections/Hero.tsx
@@ -1,10 +1,10 @@
 export default function Hero() {
   return (
-    <section className="relative mb-16 xl:mb-24">
+    <section className="relative mb-16 lg:max-h-[calc(100vh-131px)] xl:mb-24">
       <img
         src="/images/gazebo_kitchen.jpg"
         alt="image of a gazebo with outdoor kitchen"
-        className="h-full w-full object-cover"
+        className="h-full w-full object-cover lg:max-h-[calc(100vh-131px)]"
       />
       <div className="absolute left-0 top-0 h-full w-full bg-black/65"></div>
       <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center">
